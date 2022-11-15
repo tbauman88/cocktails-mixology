afbb3c8c05a6f4818d1ca4f6dcbb32668d490850
Tyler Bauman <t.bauman@vehikl.com>
2022-11-15 09:02:01 -0500

    add migrations

---
+++ database/migrations/2022_11_09_014608_create_drinks_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDrinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('drinks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('owner');
+            $table->boolean('published')->default(false);
+            $table->integer('save_count')->default(0);
+
+            $table->foreign('owner')->references('id')->on('users');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('drinks');
+    }
+}
---
+++ database/migrations/2022_11_09_014742_create_drink_ingredient_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDrinkIngredientTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('drink_ingredients', function (Blueprint $table) {
+            $table->id();
+            $table->tinyText('amount');
+            $table->tinyText('amount_unit');
+
+            $table->unsignedBigInteger('drink_id');
+            $table->foreign('drink_id')->references('id')->on('drinks');
+            $table->unsignedBigInteger('ingredient_id');
+            $table->foreign('ingredient_id')->references('id')->on('ingredients');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('drink_ingredient');
+    }
+}
